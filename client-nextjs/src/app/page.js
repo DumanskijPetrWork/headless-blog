80672f89f86c54d18dec7a431a13b59d9f9f4811
Duma <dumanskijpetr@gmail.com>
2024-12-03 01:41:59 +0500

    edit: Home page

--- client-nextjs/src/app/page.js
+++ client-nextjs/src/app/page.js
@@ -1,3 +1,20 @@
-const Home = () => <div>Home</div>;
+import Card from '@/components/card/card';
+
+const Home = () => (
+	<div className='container pb-80'>
+		<Card />
+		<div className='row'>
+			<div className='col col_4'>
+				<Card />
+			</div>
+			<div className='col col_4'>
+				<Card />
+			</div>
+			<div className='col col_4'>
+				<Card />
+			</div>
+		</div>
+	</div>
+);
 
 export default Home;
