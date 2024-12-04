88e5e061e4351c7230b30802524cb45088af29b7
Duma <dumanskijpetr@gmail.com>
2024-12-04 20:13:53 +0500

    ref: Home page

--- client-nextjs/src/app/page.jsx
+++ client-nextjs/src/app/page.jsx
@@ -1,65 +1,62 @@
 import { IconTypes } from '@/components/button/button';
 import Card from '@/components/card/card';
+import CONFIG from '@/config';
+import useFetchBlogs from '@/hooks/useFetchBlogs';
 
-const Home = () => (
-	<div className='container pb-80'>
-		<Card
-			className='mb-30'
-			label={'Product Reviews'}
-			title={
-				'Class aptent taciti sociosqu ad litora torquent per conubia nostra'
-			}
-			summary={
-				'Mauris interdum felis lacus. Praesent nulla libero, vulputate ut lorem ac, sollicitudin fringilla velit. Quisque velit elit.'
-			}
-			href={'#'}
-			btnIcon={IconTypes.ARROW_RIGHT}
-		/>
-		<div className='row'>
-			<div className='col col_4 m-mw-100'>
-				<Card
-					className='mb-30'
-					label={'Opinions'}
-					title={
-						'Class aptent taciti sociosqu ad litora torquent per conubia nostra'
-					}
-					summary={
-						'Mauris interdum felis lacus. Praesent nulla libero, vulputate ut lorem ac, sollicitudin fringilla velit. Quisque velit elit.'
-					}
-					href={'#'}
-					btnIcon={IconTypes.ARROW_RIGHT}
-				/>
-			</div>
-			<div className='col col_4 m-mw-100'>
-				<Card
-					className='mb-30'
-					label={'Product Reviews'}
-					title={
-						'Class aptent taciti sociosqu ad litora torquent per conubia nostra'
-					}
-					summary={
-						'Mauris interdum felis lacus. Praesent nulla libero, vulputate ut lorem ac, sollicitudin fringilla velit. Quisque velit elit.'
-					}
-					href={'#'}
-					btnIcon={IconTypes.ARROW_RIGHT}
-				/>
-			</div>
-			<div className='col col_4 m-mw-100'>
-				<Card
-					className='mb-30'
-					label={'Travel Guides'}
-					title={
-						'Class aptent taciti sociosqu ad litora torquent per conubia nostra'
-					}
-					summary={
-						'Mauris interdum felis lacus. Praesent nulla libero, vulputate ut lorem ac, sollicitudin fringilla velit. Quisque velit elit.'
-					}
-					href={'#'}
-					btnIcon={IconTypes.ARROW_RIGHT}
-				/>
+const Home = async () => {
+	const [featuredBlogs, blogs] = await useFetchBlogs();
+
+	return (
+		<div className='container pb-80'>
+			{featuredBlogs.map(
+				({
+					documentId,
+					category,
+					title,
+					summary,
+					featuredImage,
+					slug,
+				}) => (
+					<Card
+						className='mb-30'
+						key={documentId}
+						label={category}
+						title={title}
+						summary={summary}
+						imgSrc={CONFIG.server + featuredImage.url}
+						imgAlt='Featured Image'
+						href={'/' + slug}
+						btnIcon={IconTypes.ARROW_RIGHT}
+					/>
+				)
+			)}
+			<div className='row'>
+				{blogs.map(
+					({
+						documentId,
+						category,
+						title,
+						summary,
+						thumbnail,
+						slug,
+					}) => (
+						<div className='col col_4 m-mw-100' key={documentId}>
+							<Card
+								className='mb-30'
+								label={category}
+								title={title}
+								summary={summary}
+								imgSrc={CONFIG.server + thumbnail.url}
+								imgAlt='Thumbnail'
+								href={'/' + slug}
+								btnIcon={IconTypes.ARROW_RIGHT}
+							/>
+						</div>
+					)
+				)}
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Home;
